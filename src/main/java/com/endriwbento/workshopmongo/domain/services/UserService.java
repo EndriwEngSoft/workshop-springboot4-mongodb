09072333c58c9023d47098309923e8899f138959
Endriw Bento <endriwbento@gmail.com>
2026-02-12 21:43:30 -0300

    Inserção de usuário com POST

--- src/main/java/com/endriwbento/workshopmongo/domain/services/UserService.java
+++ src/main/java/com/endriwbento/workshopmongo/domain/services/UserService.java
@@ -1,6 +1,7 @@
 package com.endriwbento.workshopmongo.domain.services;
 
 import com.endriwbento.workshopmongo.domain.User;
+import com.endriwbento.workshopmongo.domain.dto.UserDTO;
 import com.endriwbento.workshopmongo.domain.repository.UserRepository;
 import com.endriwbento.workshopmongo.domain.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
