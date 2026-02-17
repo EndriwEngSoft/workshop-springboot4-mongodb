ee5b982b7578949aac11e75edb445eab70161a3d
Endriw Bento <endriwbento@gmail.com>
2026-02-16 21:55:23 -0300

    Obtendo um post por id

---
+++ src/main/java/com/endriwbento/workshopmongo/domain/services/PostService.java
@@ -0,0 +1,21 @@
+package com.endriwbento.workshopmongo.domain.services;
+
+import com.endriwbento.workshopmongo.domain.Post;
+import com.endriwbento.workshopmongo.domain.repository.PostRepository;
+import com.endriwbento.workshopmongo.domain.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+        public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
---
+++ src/main/java/com/endriwbento/workshopmongo/resources/PostResource.java
@@ -0,0 +1,21 @@
+package com.endriwbento.workshopmongo.resources;
+
+import com.endriwbento.workshopmongo.domain.Post;
+import com.endriwbento.workshopmongo.domain.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method=RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
